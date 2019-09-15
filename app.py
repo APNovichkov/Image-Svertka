4a04d2afda7901984668ae2ca8ae32fda5b261fc
Andrey Novichkov <andrey.novichkov@gmail.com>
2019-09-14 17:41:04 -0700

    [Addition] Made a working prototype

--- app.py
+++ app.py
@@ -1,10 +1,76 @@
 from flask import Flask, render_template
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+import numpy as np
+import os
+
+
+original_images_folder = os.path.join('static', 'original_images')
+new_images_folder = os.path.join('static', 'edited_images')
 
 app = Flask(__name__)
+app.config['DOWNLOAD_FOLDER'] = original_images_folder
+app.config['UPLOAD_FOLDER'] = new_images_folder
+
+# Filters
+
+f_outline = [
+    [-1, -1, -1],
+    [-1, 8, -1],
+    [-1, -1, -1]
+]
+
+f_sharp = [
+    [0, -1, 0],
+    [-1, 5, -1],
+    [0, -1, 0]
+]
+
+f_blur = (np.array([
+    [1, 1, 1],
+    [1, 1, 1],
+    [1, 1, 1]
+]) / 9).tolist()
+
+f_custom = (np.array([
+    [0, 0, 1],
+    [0, -1, 0],
+    [1, 0, 0]
+])).tolist()
+
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    original_img_path = os.path.join(app.config['DOWNLOAD_FOLDER'], 'la_sunset_1.jpg')
+    orignal_img = img.imread(original_img_path)
+
+    new_img = apply_filter(orignal_img, f_outline)
+    new_img = new_img.astype('uint8')
+    new_img_path = os.path.join(app.config['UPLOAD_FOLDER'], 'la_sunset_1.jpg')
+
+    img.imsave(new_img_path, new_img)
+
+    return render_template(
+        "index.html",
+        original_img_path=original_img_path,
+        new_img_path=new_img_path)
+
+
+def apply_filter(img, f):
+    ROWS = img.shape[0]
+    COLS = img.shape[1]
+
+    d = np.zeros((ROWS, COLS, 3), dtype=int).tolist()
+    for k in range(3):
+        for i in range(ROWS - 2):
+            for j in range(COLS - 2):
+                s = 0
+                for ii in range(3):
+                    for jj in range(3):
+                        s += img[i + ii][j + jj][k] * f[ii][jj]
+                d[i + 1][j + 1][k] = int(s)
+    d = np.array(d)
+    return np.clip(d, 0, 255)
 
 
 if __name__ == '__main__':
